61ea599acb9341ce7d54b08d0e7a2eb2e9b1b350
Coma <guilem.coma@blackdata.systems>
2018-12-19 17:59:59 +0100

    vue-language-translation test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <p>{{ $t("message.hello") }}</p>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
